1ab9810609fa759096b753c0d118c3cabc3782b4
dmitry-erin <dmitry.erin@unikie.com>
2024-08-19 09:16:50 +0300

    Remove test server bin
    Signed-off-by: dmitry-erin <dmitry.erin@unikie.com>

--- build.rs
+++ build.rs
@@ -8,14 +8,5 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         "control_panel_gui.gresource",
     );
 
-    //test-server needs this
-    let proto = "proto/admin.proto";
-
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-    tonic_build::configure()
-        .file_descriptor_set_path(out_dir.join("admin_descriptor.bin"))
-        .compile(&["proto/admin.proto"], &["admin"])
-        .unwrap();
-
     Ok(())
 }
