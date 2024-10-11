9d0c4db4983ff44dca5ffbea56fa04d8947c8431
Santtu Lakkala <santtu.lakkala@unikie.com>
2024-10-11 17:50:41 +0300

    Minor refactoring
    Signed-off-by: Santtu Lakkala <santtu.lakkala@unikie.com>

--- src/data_provider.rs
+++ src/data_provider.rs
@@ -1,8 +1,9 @@
 use std::cell::RefCell;
+use glib::{IsA, Object};
 use gtk::{self, gio, glib, prelude::*};
 use gio::ListStore;
 use std::thread::{self, JoinHandle};
-use std::sync::{Arc, Mutex, RwLock, mpsc::{self, Sender, Receiver}, atomic::{AtomicBool, Ordering}};
+use std::sync::{Arc, Mutex, RwLock, atomic::{AtomicBool, Ordering}};
 use tokio::time::{sleep, Duration, timeout};
 use tokio::runtime::Runtime;
 
@@ -18,22 +19,37 @@ use crate::{ADMIN_SERVICE_ADDR, ADMIN_SERVICE_PORT};
 pub mod imp {
     use super::*;
 
+    trait TypedStore<T: IsA<Object>> {
+        fn get(&self, index: u32) -> Option<T>;
+        fn typed_iter(&self) -> impl Iterator<Item = T>;
+    }
+
+    impl<T: IsA<Object>> TypedStore<T> for ListStore {
+        fn get(&self, index: u32) -> Option<T> {
+            self.item(index).and_then(|item| item.downcast().ok())
+        }
+
+        fn typed_iter(&self) -> impl Iterator<Item = T> {
+            let s = self.clone();
+            (0..).map_while(move |idx| s.get(idx))
+        }
+    }
+
     #[derive(Debug)]
     pub struct DataProvider {
-        store: Arc<Mutex<ListStore>>,
+        store: ListStore,
         //settings: Arc<Mutex<SettingsGObject>>,//will be in use in the future
         pub status: bool,
         admin_client: Arc<RwLock<AdminClient>>,
         service_address: RefCell<(String, u16)>,
         handle: RefCell<Option<JoinHandle<()>>>,
-        stop_signal: Arc<AtomicBool>,
+        stop_signal: RefCell<Option<async_channel::Sender<()>>>,
     }
 
     #[derive(Debug)]
     pub enum EventExtended {
         InitialList(Vec<QueryResult>),
         InnerEvent(Event),
-        BreakLoop,
     }
 
     impl DataProvider {
@@ -41,13 +57,13 @@ pub mod imp {
             let init_store = ListStore::new::<VMGObject>();//Self::fill_by_mock_data();
 
             Self {
-                store: Arc::new(Mutex::new(init_store)),
+                store: init_store,
                 //settings: Arc::new(Mutex::new(SettingsGObject::default())),
                 status: false,
                 admin_client: Arc::new(RwLock::new(AdminClient::new(address.clone(), port, None))),
                 service_address: RefCell::new((address, port)),
                 handle: RefCell::new(None),
-                stop_signal: Arc::new(AtomicBool::new(false)),
+                stop_signal: RefCell::new(None),
             }
         }
 
@@ -57,10 +73,9 @@ pub mod imp {
             self.service_address.borrow().1);
             let admin_client = self.admin_client.clone();
             let store = self.store.clone();
-            let stop_signal = self.stop_signal.clone();
-            self.stop_signal.store(false, Ordering::SeqCst);
 
-            let (event_tx, event_rx): (Sender<EventExtended>, Receiver<EventExtended>) = mpsc::channel();
+            let (event_tx, event_rx) = async_channel::unbounded();
+            let (quit_tx, mut quit_rx) = async_channel::bounded::<()>(1);
 
             let handle = thread::spawn(move || {
                 Runtime::new().unwrap().block_on(async move {
@@ -70,89 +85,61 @@ pub mod imp {
                     let timeout_duration = Duration::from_secs(5);
                     let Ok(Ok(result)) = timeout(timeout_duration, admin_client.read().unwrap().watch()).await 
                     else {println!("Watch call timeout/error"); return};
+                    println!("Connected!");
 
                     let list = result.initial.clone();
                     let _ = event_tx.send(EventExtended::InitialList(list));
 
-                    while !(stop_signal.load(Ordering::SeqCst)) {
-                        if let Ok(event) = result.channel.try_recv() {
-                            let _ = event_tx.send(EventExtended::InnerEvent(event));
-                        } else {
-                            println!("Error received from client lib!");
+                    loop {
+                        tokio::select! {
+                            _ = quit_rx.recv() => break,
+                            event = result.channel.recv() => {
+                                println!("Got message: {event:?}");
+                                if let Ok(event) = event {
+                                    let _ = event_tx.send(EventExtended::InnerEvent(event)).await;
+                                } else {
+                                    break;
+                                }
+                            }
                         }
-                        println!("Waiting for data...");
-                        sleep(Duration::new(2,0)).await;
                     }
-
-                    println!("BreakLoop");
-                    let _ = event_tx.send(EventExtended::BreakLoop);
                 });
             });
 
             *self.handle.borrow_mut() = Some(handle);
 
-            glib::source::idle_add_local(move || {
-                while let Ok(event_ext) = event_rx.try_recv() {
+            let store = self.store.clone();
+            glib::spawn_future_local(async move {
+                while let Ok(event_ext) = event_rx.recv().await {
                     match event_ext {
                         EventExtended::InitialList(list) => {
                             println!("Initial list: {:?}", list);
-                            let store_inner = store.lock().unwrap();
-                            store_inner.remove_all();
+                            store.remove_all();
                             for vm in list {
-                                store_inner.append(&VMGObject::new(vm.name, vm.description, vm.status, vm.trust_level));
+                                store.append(&VMGObject::new(vm.name, vm.description, vm.status, vm.trust_level));
                             }
                         },
                         EventExtended::InnerEvent(event) =>
                         match event {
                             Event::UnitStatusChanged(result) => {
                                 println!("Status: {:?}", result);
-                                let store_inner = store.lock().unwrap();
-                                for i in 0..store_inner.n_items() {
-                                    if let Some(item) = store_inner.item(i) {
-                                        let obj = item.downcast_ref::<VMGObject>().unwrap();
-                                        if obj.name() == result.name {
-                                            obj.update(result);
-                                            break;
-                                        }
-                                    }
+                                if let Some(obj) = store.typed_iter().find(|obj: &VMGObject| obj.name() == result.name) {
+                                    obj.update(result);
                                 }
-                                //for some reason func is not found
-                                /*store_inner.find_with_equal_func(|item| {
-                                    let obj = item.downcast_ref::<VMGObject>().unwrap();
-                                    if obj.name() == result.name {
-                                        obj.update(result);
-                                        true // Return true if the item is found and updated
-                                    } else {
-                                        false // Continue searching otherwise
-                                    }
-                                });*/
                             },
                             Event::UnitShutdown(result) => {
                                 println!("Shutdown info: {:?}", result);
-                                let store_inner = store.lock().unwrap();
-                                for i in 0..store_inner.n_items() {
-                                    if let Some(item) = store_inner.item(i) {
-                                        let obj = item.downcast_ref::<VMGObject>().unwrap();
-                                        if obj.name() == result.name {
-                                            obj.update(result);
-                                            break;
-                                        }
-                                    }
+                                if let Some(obj) = store.typed_iter().find(|obj: &VMGObject| obj.name() == result.name) {
+                                    obj.update(result);
                                 }
                             },
                             Event::UnitRegistered(result) => {
                                 println!("Unit registered {:?}", result);
-                                let store_inner = store.lock().unwrap();
-                                store_inner.append(&VMGObject::new(result.name, result.description, result.status, result.trust_level));
+                                store.append(&VMGObject::new(result.name, result.description, result.status, result.trust_level));
                             },
                         },
-                        EventExtended::BreakLoop => {
-                            println!("BreakLoop event received");
-                            break;
-                        }
                     }
                 }
-                glib::ControlFlow::Continue
             });
         }
 
@@ -168,10 +155,6 @@ pub mod imp {
         }
 
         pub fn get_store(&self) -> ListStore {
-            self.store.lock().unwrap().clone()
-        }
-
-        pub fn get_store_ref(&self) -> Arc<Mutex<ListStore>> {
             self.store.clone()
         }
 
@@ -190,8 +173,7 @@ pub mod imp {
         }
 
         pub fn add_vm(&self, vm: VMGObject) {
-            let store = self.store.lock().unwrap();
-            store.append(&vm);
+            self.store.append(&vm);
         }
 
         pub fn reconnect(&self, addr: Option<(String, u16)>) {
@@ -208,8 +190,9 @@ pub mod imp {
 
         pub fn disconnect(&self) {
             println!("Disconnect request...");
-            self.stop_signal.store(true, Ordering::SeqCst);
-            if let Some(handle) = self.handle.borrow_mut().take() {
+            self.stop_signal.take();
+            // self.stop_signal.store(true, Ordering::SeqCst);
+            if let Some(handle) = self.handle.take() {
                 handle.join().unwrap();
                 //drop(self.admin_client.clone());
                 println!("Client thread stopped!");
