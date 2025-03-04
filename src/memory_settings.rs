02d5fdb01631e34a62b4f3ea635cffdda860a558
Santtu Lakkala <santtu.lakkala@unikie.com>
2025-03-04 11:08:09 +0200

    Review fixes
    Signed-off-by: Santtu Lakkala <santtu.lakkala@unikie.com>

--- src/memory_settings.rs
+++ src/memory_settings.rs
@@ -1,27 +1,20 @@
-use glib::subclass::Signal;
-use glib::{Binding, Properties};
-use gtk::prelude::*;
+use glib::Binding;
 use gtk::subclass::prelude::*;
 use gtk::{glib, CompositeTemplate, Label};
 use std::cell::RefCell;
-use std::sync::OnceLock;
 
 use crate::data_provider::StatsResponse;
 
 mod imp {
     use super::*;
 
-    #[derive(Default, CompositeTemplate, Properties)]
-    #[properties(wrapper_type = super::MemorySettings)]
+    #[derive(Default, CompositeTemplate)]
     #[template(resource = "/org/gnome/controlpanelgui/ui/memory_settings.ui")]
     pub struct MemorySettings {
         pub name: String,
 
         #[template_child]
         pub data_label: TemplateChild<Label>,
-
-        #[property(name = "vm-name", get, set, type = String)]
-        footer_visible: RefCell<String>,
 
         // Vector holding the bindings to properties of `Object`
         pub bindings: RefCell<Vec<Binding>>,
@@ -35,7 +28,6 @@ mod imp {
 
         fn class_init(klass: &mut Self::Class) {
             klass.bind_template();
-            klass.bind_template_callbacks();
         }
 
         fn instance_init(obj: &glib::subclass::InitializingObject<Self>) {
@@ -43,44 +35,7 @@ mod imp {
         }
     }
 
-    #[gtk::template_callbacks]
-    impl MemorySettings {
-        /*
-        #[template_callback]
-        fn on_advanced_settings_clicked(&self) {
-            self.obj().open_advanced_settings_widget();
-        }
-        */
-    } //end #[gtk::template_callbacks]
-
-    #[glib::derived_properties]
-    impl ObjectImpl for MemorySettings {
-        fn constructed(&self) {
-            self.parent_constructed();
-
-            // After the object is constructed, bind the footer visibilty property
-            let obj = self.obj();
-            /*
-            obj.bind_property("footer-visible", &self.footer.get(), "visible")
-                .flags(glib::BindingFlags::DEFAULT)
-                .build();
-            */
-        }
-
-        fn signals() -> &'static [Signal] {
-            static SIGNALS: OnceLock<Vec<Signal>> = OnceLock::new();
-            SIGNALS.get_or_init(|| {
-                vec![
-                    /*
-                    Signal::builder("mic-changed")
-                        .param_types([u32::static_type()])
-                        .build(),
-                    */
-                ]
-            })
-        }
-    }
-
+    impl ObjectImpl for MemorySettings {}
     impl WidgetImpl for MemorySettings {}
     impl BoxImpl for MemorySettings {}
 }
