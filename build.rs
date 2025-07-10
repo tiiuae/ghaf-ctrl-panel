4a7e8284b157eab751ef4b82790e385859800702
Santtu Lakkala <santtu.lakkala@unikie.com>
2025-07-10 15:08:03 +0300

    Split bugreport to separate application

--- build.rs
+++ build.rs
@@ -4,4 +4,10 @@ fn main() {
         "src/control_panel_gui.gresource.xml",
         "control_panel_gui.gresource",
     );
+
+    glib_build_tools::compile_resources(
+        &["src/"],
+        "src/bugreport.gresource.xml",
+        "bugreport.gresource",
+    );
 }
